8f1c88353bdb044c325edce7d4b62f99c4332d4c
MSSHEN <56971308+MSSHEN@users.noreply.github.com>
2025-09-30 22:16:19 -0400

    Time: 342 ms (64.87%), Space: 0B (100%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+
+SELECT
+    visited_on,
+    amount,
+    average_amount
+FROM
+(SELECT
+    DISTINCT visited_on,
+    SUM(amount) OVER (ORDER BY visited_on RANGE BETWEEN INTERVAL '6' day PRECEDING AND CURRENT ROW) AS amount,
+    ROUND(SUM(amount) OVER (ORDER BY visited_on RANGE BETWEEN INTERVAL '6' day PRECEDING AND CURRENT ROW) /7, 2) AS average_amount
+FROM customer    )
+     AS totals
+WHERE DATEDIFF(visited_on, (SELECT MIN(visited_on) FROM customer)) >= 6
+
